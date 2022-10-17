fc791e87c39d1fdda148a8efdd6149e3c21b09c0
Hudson Tadeu <hudinvesting@gmail.com>
2022-10-17 18:38:48 -0300

    Criação de botão com data e hora

--- javascript-nao-performatico/js/script.js
+++ javascript-nao-performatico/js/script.js
@@ -7,4 +7,22 @@ function start() {
   button.addEventListener('click', handleButtonClick);
 }
 
-function handleButtonClick() {}
+function handleButtonClick() {
+  const now = new Date();
+  clickArray.push(now.toISOString());
+
+  render();
+}
+
+function render() {
+  const ul = document.querySelector('#data');
+  ul.innerHTML = '';
+
+  let lis = '';
+
+  clickArray.map((item) => {
+    lis += `<li>${item}</li>`;
+  });
+
+  ul.innerHTML = lis;
+}
